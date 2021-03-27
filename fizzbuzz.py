76ced1c909fd9512294fa647eec5f8580ee92a46
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2021-03-27 15:01:10 +0530

    Clean fizzbuzz code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,6 @@
 """Function to solve the fizzbuzz problem."""
 def fizz_buzz(num: int) -> str:
-    """
-    This is my great and neat function to solve the famous 
+    """This is my great and neat function to solve the famous 
     Fizzbuzz problem
     :param num: That is the number we want an answer for
     :return: "fizz", "buzz", "fizzbuzz" or the number itself
